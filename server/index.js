543a461016c70f9671688f9e129a490c8458471d
OnTheRocks <nathan_huber@yahoo.com>
2022-07-07 15:33:28 -0500

    created schema

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const cors = require ('cors');
 require('dotenv').config();
+const schema = require('./schema/schema');
 const port = process.env.PORT || 5400;
 const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
 
+app.use(cors());
+
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: process.env.NODE_ENV === 'development'
-}));
+})
+);
 
 app.listen(port, console.log(`Server running on port ${port}`));
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,36 @@
 const { projects, clients } = require('../sampleData');
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLList,  } = require('graphql');
 
-const { GraphQLObjectType,  } = require('graphql');
+//Clients
+const ClientType = new GraphQLObjectType({
+  name: 'Client',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString},
+    email: { type: GraphQLString},
+    phone: { type: GraphQLString}
+  })
+});
 
-//Client 23:38
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    clients: {
+      type: GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      }
+    },
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return clients.find(client => client.id === args.id);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
