21372f3c71d1bae9198afd0644e818e931bfab4c
OnTheRocks <nathan_huber@yahoo.com>
2022-08-10 00:14:16 -0500

    Created Customer page and customer table.

--- client/src/components/Customers.jsx
+++ client/src/components/Customers.jsx
@@ -1,4 +1,6 @@
 import { gql, useQuery } from '@apollo/client'
+import CustomerRow from './CustomerRow';
+
 
 const GET_CUSTOMERS = gql`
   query getCustomers {
@@ -9,13 +11,29 @@ const GET_CUSTOMERS = gql`
       phone
     }
   }
-`
+`;
 
 export const Customers = () => {
-  const { loading, error, data } = useQuery(GET_CUSTOMERS)
+  const { loading, error, data } = useQuery(GET_CUSTOMERS);
 
   if (loading) return <p>Loading....</p>;
   if (error) return <p>Something Went Wrong</p>;
 
-  return <>{!loading && !error && <h1>Customers</h1>}</>;
+  return <>{!loading && !error && (
+    <table className='table table-hover mt-3'>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.customers.map(customer=> (
+          <CustomerRow key={customer.id}  customer={customer}/>
+        ))}
+      </tbody>
+    </table>
+  )}</>;
 }
