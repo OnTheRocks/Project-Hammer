fa3e3ab0da4cdc969f287a1dd4d71c2b223ae4c1
OnTheRocks <nathan_huber@yahoo.com>
2022-08-17 15:03:54 -0500

    Setting up addCustomer MOdal.

--- client/src/components/addCustomerModal.jsx
+++ client/src/components/addCustomerModal.jsx
@@ -1,30 +1,71 @@
 import { useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { useMutation } from "@apollo/client";
+import e from "cors";
 
 export default function AddCustomerModal() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone]  = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, phone);
+  };
+
   return (
     <>
     
     {/* <!-- Button trigger modal --> */}
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
+<button type="button" 
+  className="btn btn-primary" 
+  data-bs-toggle="modal" 
+  data-bs-target="#exampleModal"
+>
+  <div className="d-flex align-items-center">
+    <FaUser className="icon" />
+    <div>Add Customer</div>
+  </div>
 </button>
 
 {/* <!-- Modal --> */}
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+<div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Add Client</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+      <div className="modal-body">
+        <form onSubmit={onSubmit}>
+          <div className="mb-3">
+            <label className="form-label">Name</label>
+            <input 
+              type="text"
+              className="form-control" id="name"
+              value={name} onChange={ (e) => setName(e.target.value) } 
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Email</label>
+            <input 
+              type="text"
+              className="form-control" id="email"
+              value={email} onChange={ (e) => setEmail(e.target.value) } 
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Phone</label>
+            <input 
+              type="text"
+              className="form-control" id="phone"
+              value={phone} onChange={ (e) => setPhone(e.target.value) } 
+            />
+          </div>
+
+          <button type="submit"
+          data-bs-dismiss="modal" className="btn btn-secondary">Submit</button>
+        </form>
       </div>
     </div>
   </div>
