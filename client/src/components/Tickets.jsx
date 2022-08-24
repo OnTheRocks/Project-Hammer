94afce43a2ccfef0e728be1e224db47dbf568ce0
OnTheRocks <nathan_huber@yahoo.com>
2022-08-24 15:11:18 -0500

    Ready to setup Router.

--- client/src/components/Tickets.jsx
+++ client/src/components/Tickets.jsx
@@ -7,12 +7,12 @@ export default function Tickets() {
   const { loading, error, data } = useQuery(GET_TICKETS);
 
   if (loading) return <Spinner />
-  if ( error ) return <p>Something Went Wrong</p>;
+  if ( error ) return <p>Something Went Wrong</p>
   
   return (
   <>
     {data.tickets.length > 0 ? (
-      <div className="row">
+      <div className="row mt-4">
         {data.tickets.map((ticket) => (
           <TicketCard key={ticket.id} ticket=
           {ticket} />
--- client/src/components/assets/TicketCard.jsx
+++ client/src/components/assets/TicketCard.jsx
@@ -1,12 +1,14 @@
 export default function TicketCard({ticket}) {
   return (
-    <div className="col-md-4">
-      <div className="card md-3">
+    <div className="col-md-6">
+      <div className="card md-3 mt-2">
         <div className="card body">
-          <div className="d-flex justify-content-between align-items-center">;
-          <h5 className="card-title">{ticket.id}</h5>
+          <div className="d-flex justify-content-between align-items-center">
+          <h5 className="card-title">{ticket.ticketNum}</h5>
 
+          <a className="btn btn-light" href={`/ticket/${ticket.id}`}>view</a>
           </div>
+          <p className="small">Ticket ID: <strong>{ticket.id}</strong></p>
         </div>
       </div>
     </div>
